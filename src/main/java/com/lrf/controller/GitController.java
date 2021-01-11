9f9b34ef54fa75bfbfa2ebd6cf8bd73a81dd7a41
aaalrf <aaa88815115@outlook.com>
2021-01-11 14:26:59 +0800

    add GitController 2021.1.11 14:26

---
+++ src/main/java/com/lrf/controller/GitController.java
@@ -0,0 +1,13 @@
+package com.lrf.controller;
+
+/**
+ * Description:
+ *
+ * @author aaa88
+ * @date 2021/1/11
+ **/
+public class GitController {
+    public static void main(String[] args) {
+        System.out.println("测试git");
+    }
+}
