744303ef343942a5dfa0af4ac7117338f9421167
aaalrf <aaa88815115@outlook.com>
2021-01-11 16:01:42 +0800

    hebingfenzhi GitController 2021.1.11 16:01

--- src/main/java/com/lrf/controller/GitController.java
+++ src/main/java/com/lrf/controller/GitController.java
@@ -9,5 +9,6 @@ package com.lrf.controller;
 public class GitController {
     public static void main(String[] args) {
         System.out.println("测试git成功lrf");
+        System.out.println("合并分支");
     }
 }
